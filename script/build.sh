bf0aa035b1b1136c91edf1d8a8f85bbb2d768a05
Ricky Cook <ricky.cook@redbubble.com>
2015-12-31 13:03:03 +1100

    Add pipeline

--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [[ -z "$KUMO_TUTUM_VERSION" && -n "$BUILDKITE_BUILD_NUMBER" ]]; then
+  export KUMO_TUTUM_VERSION="$BUILDKITE_BUILD_NUMBER"
+fi
+
 echo "--- :wind_chime: Building gem :wind_chime:"
 
 gem build kumo_tutum.gemspec
