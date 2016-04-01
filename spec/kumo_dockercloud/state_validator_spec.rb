fa0a1245da1fb20217130c8c2a0ee7ef4cf58a66
Lucas Wilson-Richter <lucas.wilsonrichter@redbubble.com>
2016-04-01 14:34:08 +1100

    The API is no longer a hash

--- spec/kumo_dockercloud/state_validator_spec.rb
+++ spec/kumo_dockercloud/state_validator_spec.rb
@@ -4,10 +4,11 @@ describe KumoDockerCloud::StateValidator do
   describe '#wait_for_state' do
     subject { state_validator.wait_for_state('done', 1) }
     let(:state_validator) { described_class.new(state_provider) }
-    let(:state_provider) { double('state_provider', call: {state: state_provider_state}) }
+    let(:state_provider) { double('state_provider', call: service) }
+    let(:service) { instance_double(DockerCloud::Service, state: service_state, name: 'service name' ) }
 
     context 'the right state' do
-      let(:state_provider_state) { 'done' }
+      let(:service_state) { 'done' }
 
       it 'succeeds immediately if the state is right' do
         subject
@@ -15,7 +16,7 @@ describe KumoDockerCloud::StateValidator do
     end
 
     context 'the wrong state' do
-      let(:state_provider_state) { 'not quite done' }
+      let(:service_state) { 'not quite done' }
 
       it 'fails after the timeout value is reached' do
         expect { subject }.to raise_error(Timeout::Error)
