acab18227c99e194b2016dda5e9b9c96b051ae14
Chris Speck <chris.speck@redbubble.com>
2016-04-07 17:02:44 +1000

    WIP

--- spec/kumo_dockercloud/deployment_spec.rb
+++ spec/kumo_dockercloud/deployment_spec.rb
@@ -5,22 +5,14 @@ describe KumoDockerCloud::Deployment do
 
     subject { described_class.new('test_stack', 1).wait_for_exit_state }
 
-    # let(:state_validator) { instance_double(KumoDockerCloud::StateValidator) }
-
-    # let(:services) { [
-    #   instance_double(DockerCloud::Service, name: 'wrong_service', uuid: 'wrong_uuid'),
-    #   instance_double(DockerCloud::Service, name: 'correct_service', uuid: 'correct_uuid'),
-    # ] }
     let(:docker_cloud_api) { instance_double(KumoDockerCloud::DockerCloudApi, services: docker_cloud_api_services) }
     let(:docker_cloud_api_services) { double("services") }
-    # let(:state_validator) { instance_double(KumoDockerCloud::StateValidator, wait_for_exit_state: nil) }
 
 
     it 'checks the exit state of the container that belongs to the named service' do
-      # expect(KumoDockerCloud::StateValidator).to receive(:new).and_return(state_validator)
-      # expect
       allow(KumoDockerCloud::DockerCloudApi).to receive(:new).and_return(docker_cloud_api)
       expect(docker_cloud_api_services).to receive(:get).with('correct uuid')
+      expect(docker_cloud_api).to receive(:service_by_stack_and_service_name).with('test_stack', 'correct_service')
 
       subject
     end
