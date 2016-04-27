9b9986028288404cf16c6b5977b10afc8d17205e
andrew jones <andrew@andrewjones.id.au>
2016-04-27 11:06:22 +1000

    Remove hidden secret injection into stackfile

--- lib/kumo_dockercloud/stack_file.rb
+++ lib/kumo_dockercloud/stack_file.rb
@@ -5,10 +5,6 @@ module KumoDockerCloud
     def self.create_from_template(stack_template, config, env_vars)
       parsed = YAML.load(ERB.new(stack_template).result(config.get_binding))
 
-      parsed[config.app_name]['environment'] ||= {}
-      parsed[config.app_name]['environment'].merge!(config.plain_text_secrets)
-      parsed[config.app_name]['environment'].merge!(env_vars.fetch(config.app_name, {}))
-
       converted_env_vars = make_all_root_level_keys_strings(env_vars)
 
       env_vars.each do |key, _|
