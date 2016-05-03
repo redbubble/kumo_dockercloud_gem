145ff5fae1233c6962f6bf44b3c230ca8802b8d3
Pat Leamon <patrick@redbubble.com>
2016-05-03 11:57:04 +1000

    Restructure spec not to use instance methods directly

--- spec/kumo_dockercloud/environment_config_spec.rb
+++ spec/kumo_dockercloud/environment_config_spec.rb
@@ -1,6 +1,3 @@
-require 'kumo_dockercloud/environment_config'
-require 'kumo_dockercloud/docker_cloud_api'
-
 describe KumoDockerCloud::EnvironmentConfig do
   let(:env_name) { 'test' }
   let(:config_path) { File.join(__dir__, '../fixtures/config') }
@@ -22,87 +19,85 @@ describe KumoDockerCloud::EnvironmentConfig do
         expect(subject).to eq env_name
       end
     end
+  end
 
-    describe '#plain_text_secrets' do
-      context 'with no encrypted secrets' do
-        let(:secrets_data) do
-          {
-            'testkey'  => 'someval',
-            'moretest' => 'otherval'
-          }
-        end
+  describe '#plain_text_secrets' do
+    subject { instance.plain_text_secrets }
 
-        let(:string) { 'plain_text_secrets' }
-
-        it do
-          expect(subject).to eq secrets_data
-        end
+    context 'with no encrypted secrets' do
+      let(:secrets_data) do
+        {
+          'testkey'  => 'someval',
+          'moretest' => 'otherval'
+        }
       end
 
-      context 'with some encrypted secrets' do
-        let(:env_name) { 'test_encrypted' }
-        let(:plain_value) { 'otherval' }
-
-        let(:kms) { double('KumoKi::KMS') }
-
-        let(:plain_data) do
-          {
-            'testkey' => 'someval',
-            'enctest' => plain_value
-          }
-        end
-
-        let(:string) { 'plain_text_secrets' }
-
-        before do
-          allow(KumoKi::KMS).to receive(:new).and_return(kms)
-          allow(kms).to receive(:decrypt).with('ZW5jcnlwdGVkIGJpbmFyeSBkYXRh').and_return(plain_value)
-        end
-
-        it do
-          expect(subject).to eq plain_data
-        end
+      it do
+        expect(subject).to eq secrets_data
       end
     end
 
-    context 'with API mocked' do
+    context 'with some encrypted secrets' do
+      let(:env_name) { 'test_encrypted' }
+      let(:plain_value) { 'otherval' }
+
+      let(:kms) { double('KumoKi::KMS') }
+
+      let(:plain_data) do
+        {
+          'testkey' => 'someval',
+          'enctest' => plain_value
+        }
+      end
+
       before do
-        allow(KumoDockerCloud::DockerCloudApi).to receive(:new).and_return docker_cloud_api
-        allow(docker_cloud_api).to receive(:services_by_stack_name).with("application-stack-name-#{env_name}").and_return(services_for_stack)
+        allow(KumoKi::KMS).to receive(:new).and_return(kms)
+        allow(kms).to receive(:decrypt).with('ZW5jcnlwdGVkIGJpbmFyeSBkYXRh').and_return(plain_value)
       end
 
-      describe '#image_name' do
-        let(:string) { 'image_name' }
+      it do
+        expect(subject).to eq plain_data
+      end
+    end
+  end
 
-        context 'when there is a service for the given stack' do
-          let(:image_name) { 'redbubble/application-stack-name:1234' }
-          let(:services_for_stack) { [ double(DockerCloud::Service, image_name: image_name) ] }
+  context 'with API mocked' do
+    before do
+      allow(KumoDockerCloud::DockerCloudApi).to receive(:new).and_return docker_cloud_api
+      allow(docker_cloud_api).to receive(:services_by_stack_name).with("application-stack-name-#{env_name}").and_return(services_for_stack)
+    end
 
-          it 'uses the pre-exisiting image name' do
-            expect(subject).to eq image_name
-          end
-        end
+    describe '#image_name' do
+      subject { instance.image_name }
 
-        context 'when there is no service for the given stack' do
-          let(:services_for_stack) { [] }
+      context 'when there is a service for the given stack' do
+        let(:image_name) { 'redbubble/application-stack-name:1234' }
+        let(:services_for_stack) { [ double(DockerCloud::Service, image_name: image_name) ] }
 
-          it { expect(subject).to eq 'redbubble/application-stack-name:master' }
+        it 'uses the pre-exisiting image name' do
+          expect(subject).to eq image_name
         end
       end
 
-      describe '#image_tag' do
-        let(:services_for_stack) { [ double(DockerCloud::Service, image_name: image_name)] }
+      context 'when there is no service for the given stack' do
+        let(:services_for_stack) { [] }
 
-        subject { instance.image_tag }
+        it { expect(subject).to eq 'redbubble/application-stack-name:master' }
+      end
+    end
 
-        context "when the image name is 'redbubble/assete-wala:latest'" do
-          let(:image_name) { 'redbubble/application-stack-name:latest' }
-          it { expect(subject).to eq 'latest' }
-        end
-        context "when the image name is 'some-registry/mything:9999'" do
-          let(:image_name) { 'some-registry/mything:9999' }
-          it { expect(subject).to eq '9999' }
-        end
+    describe '#image_tag' do
+      let(:services_for_stack) { [ double(DockerCloud::Service, image_name: image_name)] }
+
+      subject { instance.image_tag }
+
+      context "when the image name is 'redbubble/assete-wala:latest'" do
+        let(:image_name) { 'redbubble/application-stack-name:latest' }
+        it { expect(subject).to eq 'latest' }
+      end
+      context "when the image name is 'some-registry/mything:9999'" do
+        let(:image_name) { 'some-registry/mything:9999' }
+        it { expect(subject).to eq '9999' }
       end
     end
   end
