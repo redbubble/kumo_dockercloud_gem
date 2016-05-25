9dbf80832af0b7d46ed0b8a0395553bf0ddef57f
Craig Read <craigabread@gmail.com>
2016-05-25 14:18:44 +1000

    moved event handling code into private methods

--- lib/kumo_dockercloud/haproxy_command.rb
+++ lib/kumo_dockercloud/haproxy_command.rb
@@ -11,25 +11,32 @@ module KumoDockerCloud
       cmd = %(sh -c "echo #{command} | nc -U /var/run/haproxy.stats")
       api = DockerCloud::ContainerStreamAPI.new(@container_id, cmd, @dc_client.headers, @dc_client)
 
-      api.on(:open) { |_| @acc_data = '' }
-
-      api.on(:message) do |event|
-        data = JSON.parse(event.data)['output']
-        @acc_data << data
-      end
-
-      api.on(:error) { |event|
-        raise HaproxySocketError.new(event.message)
-      }
-
-      api.on(:close) do |_|
-        puts 'Socket Closed'
-        @api = nil
-        EventMachine.stop
-      end
+      api.on(:open) { |event| on_open(event) }
+      api.on(:message) { |event| on_message(event) }
+      api.on(:error) { |event| on_error(event) }
+      api.on(:close) { |event| on_close(event) }
 
       api.run!
       @acc_data
     end
+  end
+
+  private
+
+  def on_open(_)
+    @acc_data = ''
+  end
+
+  def on_message(event)
+    data = JSON.parse(event.data)['output']
+    @acc_data << data
+  end
+
+  def on_error(event)
+    raise HaproxySocketError.new(event.message)
+  end
+
+  def on_close(event)
+    EventMachine.stop
   end
 end
