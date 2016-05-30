e6b3146a9fecc92f9d9e0abbe62fdfd26b838540
Chris Speck <chris.speck@redbubble.com>
2016-05-30 10:34:38 +1000

    Delete the guts of haproxy service that was moved to haproxy container

--- lib/kumo_dockercloud/haproxy_service.rb
+++ lib/kumo_dockercloud/haproxy_service.rb
@@ -11,16 +11,5 @@ module KumoDockerCloud
     def disable_service(service)
     end
 
-    def stats
-      CSV.parse(HaproxyCommand.new(@container_id, @client).execute('show stat'), headers: true)
-    end
-
-    def disable_server(server_name)
-      HaproxyCommand.new(@container_id, @client).execute("disable server #{server_name}")
-    end
-
-    def enable_server(server_name)
-      HaproxyCommand.new(@container_id, @client).execute("enable server #{server_name}")
-    end
   end
 end
--- spec/kumo_dockercloud/haproxy_service_spec.rb
+++ spec/kumo_dockercloud/haproxy_service_spec.rb
@@ -3,40 +3,4 @@ require 'spec_helper'
 describe KumoDockerCloud::HaproxyService do
   subject { KumoDockerCloud::HaproxyService.new('service_name') }
 
-  let(:haproxy_command) { instance_double(KumoDockerCloud::HaproxyCommand, :haproxy_command )}
-  let(:csv_output) { 'a,b\n1,2' }
-  before do
-    allow(KumoDockerCloud::HaproxyCommand).to receive(:new).and_return(haproxy_command)
-  end
-
-  describe '#stats' do
-    it 'uses haproxy_command to do the execution' do
-      expect(haproxy_command).to receive(:execute).with('show stat').and_return(csv_output)
-      subject.stats
-    end
-
-    it 'parses the output as CSV' do
-      allow(haproxy_command).to receive(:execute).with('show stat').and_return(csv_output)
-      expect(CSV).to receive(:parse).with(csv_output, headers: true)
-      subject.stats
-    end
-  end
-
-  describe '#disable_server' do
-    let(:server_name) { 'green_server' }
-
-    it 'runs disable server' do
-      expect(haproxy_command).to receive(:execute).with("disable server #{server_name}")
-      subject.disable_server(server_name)
-    end
-  end
-
-  describe '#enable_server' do
-    let(:server_name) { 'blue_server' }
-
-    it 'runs enable server' do
-      expect(haproxy_command).to receive(:execute).with("enable server #{server_name}")
-      subject.enable_server(server_name)
-    end
-  end
 end
