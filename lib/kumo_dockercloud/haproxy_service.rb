94ef1e66ee421f77a65e6b835f90ec98b302def7
andrew jones <andrew@andrewjones.id.au>
2016-06-01 09:44:52 +1000

    Allow the HAProxy service to enable backend services

--- lib/kumo_dockercloud/haproxy_service.rb
+++ lib/kumo_dockercloud/haproxy_service.rb
@@ -14,7 +14,16 @@ module KumoDockerCloud
 
       haproxy_threads = haproxy_containers.map { |haproxy_container| Thread.new { haproxy_container.disable_server(service_to_disable) } }
       haproxy_threads.each(&:join)
+    end
 
+    def enable_service(service)
+      service_to_enable = service.name
+      haproxy_containers = containers.map { |container| HaproxyContainer.new(container.uuid, @client) }
+
+      raise KumoDockerCloud::HAProxyStateError.new('Could not get instances of the haproxy container for this environment') if haproxy_containers.empty?
+
+      haproxy_threads = haproxy_containers.map { |haproxy_container| Thread.new { haproxy_container.enable_server(service_to_enable) } }
+      haproxy_threads.each(&:join)
     end
   end
 end
--- spec/kumo_dockercloud/haproxy_service_spec.rb
+++ spec/kumo_dockercloud/haproxy_service_spec.rb
@@ -1,28 +1,42 @@
 require 'spec_helper'
 
 describe KumoDockerCloud::HaproxyService do
-  subject { KumoDockerCloud::HaproxyService.new('stack_name') }
+  let(:service_name) { 'the-service' }
+  let(:container_uuid) { 'i-am-the-container'}
+  let(:docker_cloud_container) { double(:docker_cloud_haproxy, uuid: container_uuid)}
+  let(:containers) { [docker_cloud_container] }
+  let(:docker_cloud_service) { double(:service, containers: containers) }
+  let(:haproxy_container) { instance_double(KumoDockerCloud::HaproxyContainer, :haproxy_container, disable_server: nil, enable_server: nil) }
+  let(:docker_cloud_client) { double }
+  let(:kumo_docker_cloud_api) { instance_double(KumoDockerCloud::DockerCloudApi, :docker_cloud_api, client: docker_cloud_client ) }
+
+  before do
+    allow(KumoDockerCloud::DockerCloudApi).to receive(:new).and_return(kumo_docker_cloud_api)
+    allow(kumo_docker_cloud_api).to receive(:service_by_stack_and_service_name).with('stack_name', 'haproxy').and_return(docker_cloud_service)
+    allow(KumoDockerCloud::HaproxyContainer).to receive(:new).with(container_uuid, docker_cloud_client).and_return(haproxy_container)
+  end
 
   describe '#disable_service' do
     subject { KumoDockerCloud::HaproxyService.new('stack_name').disable_service(service_to_disable) }
-    let(:service_name) { 'the-service' }
     let(:service_to_disable) { instance_double(KumoDockerCloud::Service, :service_to_disable, name: service_name) }
-    let(:container_uuid) { 'i-am-the-container'}
-    let(:docker_cloud_container) { double(:docker_cloud_haproxy, uuid: container_uuid)}
-    let(:containers) { [docker_cloud_container] }
-    let(:docker_cloud_service) { double(:service, containers: containers) }
-    let(:haproxy_container) { instance_double(KumoDockerCloud::HaproxyContainer, :haproxy_container, disable_server: nil) }
-    let(:docker_cloud_client) { double }
-    let(:kumo_docker_cloud_api) { instance_double(KumoDockerCloud::DockerCloudApi, :docker_cloud_api, client: docker_cloud_client ) }
-
-    before do
-      allow(KumoDockerCloud::DockerCloudApi).to receive(:new).and_return(kumo_docker_cloud_api)
-      allow(kumo_docker_cloud_api).to receive(:service_by_stack_and_service_name).with('stack_name', 'haproxy').and_return(docker_cloud_service)
-    end
 
     it 'sends the disable_server message to all its containers' do
-      allow(KumoDockerCloud::HaproxyContainer).to receive(:new).with(container_uuid, docker_cloud_client).and_return(haproxy_container)
       expect(haproxy_container).to receive(:disable_server).with(service_name)
+      subject
+    end
+
+    it 'blows up when there are no instances of haproxy' do
+      allow(docker_cloud_service).to receive(:containers).and_return([])
+      expect { subject }.to raise_error(KumoDockerCloud::HAProxyStateError, 'Could not get instances of the haproxy container for this environment')
+    end
+  end
+
+  describe '#enable_service' do
+    subject { KumoDockerCloud::HaproxyService.new('stack_name').enable_service(service_to_enable) }
+    let(:service_to_enable) { instance_double(KumoDockerCloud::Service, :service_to_enable, name: service_name) }
+
+    it 'sends the enable_server message to all its containers' do
+      expect(haproxy_container).to receive(:enable_server).with(service_name)
       subject
     end
 
